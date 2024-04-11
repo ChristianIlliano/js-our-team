51096279312776639528abaa2a06cc625c671f29
ChristianIlliano <grafica.illiano@gmail.com>
2024-04-11 16:42:04 +0200

    feat:fix diclaration

--- js/script.js
+++ js/script.js
@@ -1,34 +1,35 @@
+"use strict";
 const ourTeam = [
     {
         name: "Wayne Barnett",
         profession: "Founder & CEO",
-        img: "wayne-barnett-founder-ceo.jpg",
+        img: "wayne-barnett-founder-ceo.jpg"
     },
     {
         name: "Angela Caroll",
         profession: "Chief Editor",
-        img: "angela-caroll-chief-editor.jpg",
+        img: "angela-caroll-chief-editor.jpg"
     },
     {
         name: "Walter Gordon",
         profession: "Office Manager",
-        img: "walter-gordon-office-manager.jpg",
+        img: "walter-gordon-office-manager.jpg"
     },
     {
         name: "Angela Lopez",
         profession: "Social Media Manager",
-        img: "angela-lopez-social-media-manager.jpg",
+        img: "angela-lopez-social-media-manager.jpg"
     },
     {
         name: "Scott Estrada",
         profession: "Developer",
-        img: "scott-estrada-developer.jpg",
+        img: "scott-estrada-developer.jpg"
     },
     {
         name: "Barbara Ramos",
         profession: "Graphic Designer",
-        img: "barbara-ramos-graphic-designer.jpg",
-    },
+        img: "barbara-ramos-graphic-designer.jpg"
+    }
 ];
 
 for (let i = 0; i < ourTeam.length; i++) {
@@ -40,7 +41,7 @@ for (let i = 0; i < ourTeam.length; i++) {
 
 const cardRow = document.getElementById("cards");
 for (let i = 0; i < ourTeam.length; i++) {
-    curMember = ourTeam[i]
+    const curMember = ourTeam[i]
     cardRow.innerHTML += `
     <div class="col justify-content-center d-flex text-center mb-4">
     <div class="card" style="width: 18rem;">

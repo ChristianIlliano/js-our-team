5b7cda18c3160c5fbb7a3651d2c9be5586c97f80
ChristianIlliano <grafica.illiano@gmail.com>
2024-04-11 15:21:58 +0200

    feat:scaffolding

---
+++ js/script.js
@@ -0,0 +1,38 @@
+const ourTeam = [
+    {
+        name: "Wayne Barnett",
+        profession: "Founder & CEO",
+        img: "wayne-barnett-founder-ceo.jpg",
+    },
+
+    {
+        name: "Angela Caroll",
+        profession: "Chief Editor",
+        img: "angela-caroll-chief-editor.jpg",
+    },
+
+    {
+        name: "Walter Gordon",
+        profession: "Office Manager",
+        img: "walter-gordon-office-manager.jpg",
+    },
+
+    {
+        name: "Angela Lopez",
+        profession: "Social Media Manager",
+        img: "angela-lopez-social-media-manager.jpg",
+    },
+    {
+        name: "Scott Estrada",
+        profession: "Developer",
+        img: "scott-estrada-developer.jpg",
+    },
+
+    {
+        name: "Barbara Ramos",
+        profession: "Graphic Designer",
+        img: "barbara-ramos-graphic-designer.jpg",
+    },
+]
+
+console.log(ourTeam.name, ourTeam.profession, ourTeam.img);
